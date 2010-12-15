cf0bf21711ddad43f7f801486a8e4651e068ec29
moro <moronatural@gmail.com>
2010-12-15 17:55:44 +0900

    unite-script interface.

---
+++ lib/tsuridana/runner/unite.rb
@@ -0,0 +1,35 @@
+# coding: utf-8
+require 'tsuridana/step_definitions'
+
+module Tsuridana
+  module Runner
+    class Unite
+      def self.run(*files)
+        files.each{|f| new(f).display }
+      end
+
+      def initialize(src)
+        @step_definitions = Tsuridana::StepDefinitions.new(src)
+      end
+
+      def steps
+        unless @parsed
+          @step_definitions.parse('')
+          @parsed = true
+        end
+        @step_definitions.step_definitions
+      end
+
+      def display
+        steps.each do |step|
+          puts "#{filter(step.re_src)}\t:edit +#{step.lino} #{@step_definitions.src}"
+        end
+      end
+
+      private
+      def filter(re_src)
+        re_src.sub(/^\^(.+)\$$/, '\1')
+      end
+    end
+  end
+end
